7c7cd2fe359b10150e9756f2dd80f05a978047f0
thabatagomes <159185229+thabatagomes@users.noreply.github.com>
2025-04-01 11:21:23 -0300

    01-04

---
+++ pagina-assesivel-main/sript.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() { 
+const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById ('aumentar-fonte');
+
+let tamanhoAtualFonte= 1;
+
+aumentaFonteBotao.addEventListener('click', function(){
+ tamanhoAtualFonte + = 0,1;
+ document.body.style.fontSize = '$(tamanhoAtualFonte)rem'
+
+
+}
+
+
+)
+
+
+}
+)
--- sript.js
+++ sript.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() { 
-const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
-const diminuiFonteBotao = document.getElementById ('aumentar-fonte');
+document.addEventListener('DOMContentLoaded',function(){
 
-let tamanhoAtualFonte= 1;
-
-aumentaFonteBotao.addEventListener('click', function(){
- tamanhoAtualFonte + = 0,1;
- document.body.style.fontSize = '$(tamanhoAtualFonte)rem'
-
-
-}
-
-
-)
-
-
-}
-)
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    
+    let tamanhoAtualFonte= 1;
+    
+    aumentaFonteBotao.addEventListener('click', function () {
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    
+    })
+    
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        
+        })
+    
+    })
