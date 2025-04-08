065e940f41f96bf52704dae438cb0664c626042d
thabatagomes <159185229+thabatagomes@users.noreply.github.com>
2025-04-08 11:20:15 -0300

    08/04

--- sript.js
+++ sript.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
@@ -16,5 +18,9 @@ document.addEventListener('DOMContentLoaded',function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
         
         })
-    
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
     })
+
+})
+
