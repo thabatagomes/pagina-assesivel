2d4393d3486ea6bda7960a32b438b33cec40b6f4
thabatagomes <159185229+thabatagomes@users.noreply.github.com>
2025-04-09 11:17:42 -0300

    09-04

--- sript.js
+++ sript.js
@@ -2,18 +2,26 @@ document.addEventListener('DOMContentLoaded',function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+   
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    }
+    )
+    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
     
     let tamanhoAtualFonte= 1;
     
-    aumentaFonteBotao.addEventListener('click', function () {
+    aumentarFonteBotao.addEventListener('click', function () {
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     
     })
     
-    diminuiFonteBotao.addEventListener('click', function () {
+    diminuirFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
         
